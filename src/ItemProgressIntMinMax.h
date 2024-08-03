ffacf5552f77a408394bb4a1c5f8f3e67a29eeb1
Oberoner21 <t.schindel@gmx.de>
2024-08-03 15:25:14 +0200

    simplification

--- src/ItemProgressIntMinMax.h
+++ src/ItemProgressIntMinMax.h
@@ -16,72 +16,57 @@
 
 /**
  * @class ItemProgressIntMinMax
- * 
+ *
  * @brief ItemProgressIntMinMax
  *  indicates that the current item is a list.
  */
 class ItemProgressIntMinMax : public MenuItem
 {
-   private:
-    fptrMapping mapping = NULL;  ///< Pointer to a mapping function
-    fptrInt callback = NULL;     ///< Pointer to a callback function
-    uint16_t progress = 0;       ///< The progress
-    uint8_t stepLength = 1;
-    uint16_t minval;
-    uint16_t maxval;
-    uint16_t correction = 0;     ///< Correction value for use negative numbers.                                    
-    const char *measure = NULL;  ///< Pointer to a measure string
+    private:
+        fptrInt callback = NULL; ///< Pointer to a callback function
+        uint16_t progress = 0;   ///< The progress
+        uint8_t stepLength = 1;
+        uint16_t minval;
+        uint16_t maxval;
+        const char *measure = NULL; ///< Pointer to a measure string
 
-   public:
-    /**
-     * @brief Constructs a new ItemProgressIntMinMax object.
-     *
-     * @param key The key of the menu item.
-     * @param measure The measure of the menu item
-     * @param minval The minimal progress value.
-     * @param maxval The maximal progress value.
-     * @param start The starting progress value.
-     * @param stepLength The length of steps to the next position.
-     * @param callback A pointer to the callback function to execute when this menu item is selected.
-     */
-    ItemProgressIntMinMax
-    (const char* key, const char *measure, int16_t minval, int16_t maxval, int16_t start, uint8_t stepLength,
-                 fptrInt callback)
-        : MenuItem(key, MENU_ITEM_PROGRESS),
-          callback(callback),
-          stepLength(stepLength),
-          measure(measure)
-    {
-        if(minval < 0)
-        {
-            this->correction = abs(minval);
-            this->minval = 0;
-            this->maxval = maxval + correction;
-            this->progress = start + correction;
-        }
-        else 
-        {
-            this->minval = minval;
-            this->maxval = maxval;
-            this->progress = start;
-        }
-    }
+    public:
+        /**
+         * @brief Constructs a new ItemProgressIntMinMax object.
+         *
+         * @param key The key of the menu item.
+         * @param measure The measure of the menu item
+         * @param minval The minimal progress value.
+         * @param maxval The maximal progress value.
+         * @param start The starting progress value.
+         * @param stepLength The length of steps to the next position.
+         * @param callback A pointer to the callback function to execute when this menu item is selected.
+         */
+        ItemProgressIntMinMax(const char *key, const char *measure, uint16_t minval, uint16_t maxval, uint16_t start, uint8_t stepLength,
+                            fptrInt callback)
+            : MenuItem(key, MENU_ITEM_PROGRESS),
+            callback(callback),
+            progress(start),
+            stepLength(stepLength),
+            minval(minval),
+            maxval(maxval),
+            measure(measure) {}
 
     /**
-     * @brief Increments the progress of the list.
+     * @brief Increments the progress value.
      */
     void increment() override
     {
-        if (progress >= maxval) return;
+        if ((int16_t)progress >= (int16_t)maxval) return;
         progress += stepLength;
     }
 
     /**
-     * @brief Decrements the progress of the list.
+     * @brief Decrements the progress value.
      */
-    void decrement() override 
+    void decrement() override
     {
-        if (progress <= minval) return;
+        if ((int16_t)progress <= (int16_t)minval) return;
         progress -= stepLength;
     }
 
@@ -89,16 +74,16 @@ class ItemProgressIntMinMax : public MenuItem
      * Set the progress on the item
      * @param uint16_t progress for the item
      */
-    void setProgress(uint16_t value) override 
+    void setProgress(uint16_t value) override
     {
-        if (progress < minval || progress > maxval) return;
-        progress = value + correction;  // correct negative values
+        if ((int16_t)progress < (int16_t)minval || (int16_t)progress > (int16_t)maxval) return;
+        progress = value;
     }
 
     /**
-     * Return the progress
+     * Return the progress with correction then use negative values
      */
-    uint16_t getItemIndex() override { return progress - correction; }
+    uint16_t getItemIndex() override { return progress; }
 
     /**
      * Return the callback
@@ -107,32 +92,24 @@ class ItemProgressIntMinMax : public MenuItem
 
     /**
      * @brief Returns the value to be displayed.
-     *        If there's no mapping, it returns the progress
-     *        and if measure not NULL then concate the measure
-     *        string behind the progress
-     *        else it returns the mapped progress
+     *        Returns the progress and if measure not
+     *        NULL then adds the measure string behind the
+     *        progress value.
      *
      * @return A pointer to the value of the current list item.
      */
-    char* getValue() override
+    char *getValue() override
     {
-        if (mapping == NULL)
+        static char buffer[10];
+        itoa((int16_t)progress, buffer, 10);
+        if (measure != NULL)
         {
-            static char buffer[10];
-            itoa((int)progress - correction, buffer, 10);
-            if(measure != NULL)
-            {
-                concat(buffer, measure, buffer);
-            }
-            return buffer;
+            concat(buffer, measure, buffer);
         }
-        else 
-        {
-            return mapping(progress);
-        }
+        return buffer;
     }
 };
 
 #define ITEM_PROGRESS_INT_MINMAX(...) (new ItemProgressIntMinMax(__VA_ARGS__))
 
-#endif  // ItemProgress_H
+#endif // ItemProgress_H
